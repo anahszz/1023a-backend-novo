50f3c3e4f946b14952f60e07b7680c1273ea5613
anahszz <ana.lima15@estudante.ifms.edu.br>
2025-09-15 09:06:05 -0400

    atualização do script de start para utilizar o mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,63 @@
 import 'dotenv/config'
-import mysql from'mysql2/promise';
+import mysql from 'mysql2/promise';
 console.log(process.env.DBUSER);
 
-import express, {Request,Response} from 'express';
+import express, { Request, Response } from 'express';
 const app = express();
 
-app.get ('/', async (req:Request, res:Response) => {
-    if(!process.env.DBUSER){//! significa que é a negação da variável
-        res.status(500).send("Variável de ambiente DBUSER não está definida");
+app.get('/', async (req: Request, res: Response) => {
+    if (!process.env.DBUSER) {//! significa que é a negação da variável
+        res.status(500).send("Variável de ambiente DBUSER não está definida")
         return;
     }
-    if(process.env.DBPASSWORD==undefined){
-        res.status(500).send("Variável de ambiente DBPASSWORD não está definida");  
+    if (process.env.DBPASSWORD==undefined) {
+        res.status(500).send("Variável de ambiente DBPASSWORD não está definida")
         return;
     }
-    if(!process.env.DBHOST){
-        res.status(500).send("Variável de ambiente DBHOST não está definida");  
+    if (!process.env.DBHOST) {
+        res.status(500).send("Variável de ambiente DBHOST não está definida")
         return;
     }
-    if(!process.env.DBNAME){
-        res.status(500).send("Variável de ambiente DBNAME não está definida");  
+    if (!process.env.DBNAME) {
+        res.status(500).send("Variável de ambiente DBNAME não está definida")
         return;
     }
-    if(!process.env.DBPORT){
-        res.status(500).send("Variável de ambiente DBPORT não está definida");  
+    if (!process.env.DBPORT) {
+        res.status(500).send("Variável de ambiente DBPORT não está definida")
         return;
     }
-    try{
-    const connection = await mysql.createConnection({
-        host: process.env.DBHOST,   
-        user: process.env.DBUSER,
-        password: process.env.DBPASSWORD,
-        database: process.env.DBNAME,
-        port: Number(process.env.DBPORT)
-    });
-    res.send("Conectado ao banco de dados com sucesso!");
-    await connection.end();
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DBHOST,
+            user: process.env.DBUSER,
+            password: process.env.DBPASSWORD,
+            database: process.env.DBNAME,
+            port: Number(process.env.DBPORT)
+        })
+        res.send("Conectado ao banco de dados com sucesso!");
+        await connection.end();
     }
-    catch(error){
+    catch (error) {
         res.status(500).send("Erro ao conectar ao banco de dados: " + error);
     }
-
 });
+
+//criar uma rota get para produtos que retorne  a lista de produtos do banco de dados
+//o produto deve ter id, nome, preço, urlfoto, descrição
+//deve se criar uma tabela no banco de dados AIVEN para armazenar os produtos
+//a resposta deve ser um array de produtos em formato JSON
+/*
+
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    urlfoto VARCHAR(255),
+    descricao TEXT
+);
+fazer pelo menos 3 inserções nessa tabela
+*/
+
 app.listen(8000, () => {
-    console.log('Server is running on port 8000')
+    console.log('Server is running on port 8000');
 });
