04ee21615526a51e534987c40f25cd40603538bf
anahszz <ana.lima15@estudante.ifms.edu.br>
2025-10-14 12:48:25 -0300

    mexendo no token

--- src/middleware/Auth.ts
+++ src/middleware/Auth.ts
@@ -1,8 +1,26 @@
-import {Request, Response, NextFunction} from 'express'
-function Auth(req:Request,res:Response,next:NextFunction){
-    console.log("Passei no middleware")
-    next()
-    //return res.status(401).json({mensagem:"Você não tem permissão para acessar esse recurso!"})
+import jwt from "jsonwebtoken"
+import { Request, Response, NextFunction } from 'express'
+
+interface ResquestAuth extends Request {
+    usuarioId?: string
+}
+
+function Auth(req: ResquestAuth, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization
+    if (!authHeader)
+        return res.status(401).json({ mensagem: "Token não fornecido!" })
+    const token = authHeader.split("")[1]!
+    jwt.verify(token, process.env.JWT_SECRET!, (err, decoded) => {
+        if (err) {
+            console.log(err)
+            return res.status(401).json({ mensagem: "Token inválido!" })
+        }
+        if (typeof decoded === "string" || !decoded || !("usuarioId" in decoded))
+            return res.status(401).json({ mensagem: "Token inválido!" })
+
+        req.usuarioId = decoded.usuarioId;
+        next()
+    })
 }
 
 export default Auth
